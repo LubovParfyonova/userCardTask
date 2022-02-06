32cbfb95c8bd59ece4847a3a9d0f184ed63033a9
Lubov <lubaparfyonova@gmail.com>
2022-02-06 19:37:57 +0200

    Create reposite

---
+++ assets/js/index.js
@@ -0,0 +1,132 @@
+'use strict'
+
+const root = document.querySelector('#root');
+
+/* const mapSocialIcons = new Map();
+
+mapSocialIcons.set('www.facebook.com', 'assets/images/facebook.svg');
+mapSocialIcons.set('www.instagram.com', 'assets/images/instagram.svg');
+mapSocialIcons.set('twitter.com', 'assets/images/twitter.svg');
+
+ */
+
+// const HTMLLiElements = responseData.filter((user) => user.firstName && user.id && user.description).map((user)=>createUserCard(user));
+
+const HTMLLiElements = responseData.map((user)=>createUserCard(user));
+
+root.append(...HTMLLiElements)
+
+
+function createUserCard(user) {
+
+    const h3 = createElement('h3', {classNames: ['cardName']}, document.createTextNode(`${user.firstName} ${user.lastName}`));
+
+    const lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio quas repudiandae sed reiciendis modi reprehenderit rem, iste cupiditate'
+
+    const p = createElement('p', {classNames: ['cardDescription']}, document.createTextNode(lorem));
+
+    const article = createElement('article', 
+    {classNames: ['cardConteiner']}, createImageWrapper(user), h3, p);
+
+
+    return createElement('li', {classNames: ['cardWrapper']}, article);
+}
+
+/**
+ * 
+ * @param {string} type 
+ * @param {Object} options
+ *  @param {sting[]} options.classNames
+ *  @param {function} options.onClick
+ * @param {object} options.attributes
+ * @param {Node} children
+ * @returns {HTMLElement} 
+ */
+/*
+Example ob ATTRIBUTES object
+{
+    src: 'https://links.com',
+    alt: 'text',
+    title: 'description',
+}
+
+*/
+
+function createElement(type, {classNames=[], onClick = ()=>{}, attributes = {}}, ...children) {
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.onclick = onClick;
+
+    for (const [attrName, attrValue] of Object.entries(attributes)){
+        elem.setAttribute(attrName, attrValue);
+    }
+
+    elem.append(...children);
+    return elem;
+}
+
+
+
+function createImage(user) {
+    const image = document.createElement('img');
+    image.classList.add('cardImage');
+    image.setAttribute('alt', user.firstName);
+    image.setAttribute('src', user.profilePicture);
+    image.dataset.id = user.id;
+
+    image.addEventListener('error', handleImageError);
+    image.addEventListener('load', handleImageLoad);
+
+    return image;
+}
+
+function createImageWrapper(user) {
+const imageWrapper = document.createElement('div');
+imageWrapper.classList.add('cardImageWrapper');
+imageWrapper.style.backgroundColor = stringToColor(user.firstName);
+imageWrapper.setAttribute('id', `wrapper${user.id}`);
+
+const initials = document.createElement('div');
+initials.classList.add('initials');
+initials.append(document.createTextNode(user.firstName[0] || ''));
+createImage(user)
+imageWrapper.append(initials);
+
+return imageWrapper;
+}
+
+
+
+
+/* EVENT HANDLERS */
+
+function handleImageError({target}) {
+        target.remove();
+}
+
+function handleImageLoad(event) {
+    const {target, 
+        target: {
+            dataset: {
+                id
+            }
+        }
+    } = event;
+    document.getElementById(`wrapper${id}`).append(target);
+}
+
+
+/* Utils */
+
+function stringToColor (str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+        let value = (hash >> (i * 8)) & 0xFF;
+        colour += ('00' + value.toString(16)).substr(-2);
+    }
+    return colour;
+}
---
+++ assets/js/solution.js
@@ -0,0 +1,14 @@
+// new URL
+
+//new Map
+//.set('facebook.com', 'img_facebook.jpg');
+//.set('instagram.com', 'img_insta.jpg');
+//.set('twitter.com', 'img_twitter.jpg');
+
+const mapSocialIcons = new Map();
+
+mapSocialIcons.set('www.facebook.com', 'assets/images/facebook.svg');
+mapSocialIcons.set('www.instagram.com', 'assets/images/instagram.svg');
+mapSocialIcons.set('twitter.com', 'assets/images/twitter.svg');
+
+
